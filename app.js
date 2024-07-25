9bcb8f9eb98435c1225071db4b7b253cd750183b
anri103 <ianrianri@gmail.com>
2024-07-25 17:19:32 +0300

    -

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-// Закрывает мобильное меню после перехода по ссылке в нем
-const navbarToggler = document.body.querySelector('.navbar-toggler');
-const responsiveNavItems = [].slice.call( document.querySelectorAll('#navbarNav .nav-link'));
-responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener('click', () => {
-        if (window.getComputedStyle(navbarToggler).display !== 'none') {
-            navbarToggler.click();
-        }
-    });
-});
-
 // swiperAbout
 const swiperAbout = new Swiper('.swiperAbout', {
 	loop: true,
@@ -20,47 +9,16 @@ const swiperAbout = new Swiper('.swiperAbout', {
 	effect: 'fade',
 });
 
-// swiperPrice
-const swiperPrice = new Swiper('.swiperPrice', {
-	slidesPerView: 'auto',
-	spaceBetween: 16,
-	loop: false,
-	grabCursor: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-    },
-	breakpoints: {
-		992: {
-			spaceBetween: 32,
-		}
-	},
-});
-
 // swiperTestimonial
 const swiperTestimonial = new Swiper('.swiperTestimonial', {
 	slidesPerView: 1,
 	spaceBetween: 16,
 	loop: true,
 	grabCursor: true,
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
 	pagination: {
         el: '.swiper-pagination',
         dynamicBullets: true,
     },
-	breakpoints: {
-		992: {
-			slidesPerView: 4,
-			spaceBetween: 32,
-		}
-	},
 });
 
 // fancybox
